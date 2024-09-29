f568cfffa7e2bd53fba073b23f858a3724c87024
YasmineBelmiro <devyasminebelmiro@gmail.com>
2024-09-29 11:41:26 -0300

    commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,64 @@
+import org.jetbrains.kotlin.storage.CacheResetOnProcessCanceled.enabled
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+
+    namespace = "ifgoiano.urt.cronometro"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "ifgoiano.urt.cronometro"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    // Habilitar View Binding e Data Binding
+    viewBinding {
+        enable = true
+    }
+
+    // Habilitar Data Binding
+    dataBinding {
+        //noinspection DataBindingWithoutKapt
+        enabled = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
